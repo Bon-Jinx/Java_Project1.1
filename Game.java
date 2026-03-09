dccca48d9005b0ef1a4f91c3a5f29189d9480086
Bon-Jinx <johnkyledevilleres2026@gmail.com>
2026-03-09 14:07:14 +0000

    Game class

--- Game.java
+++ Game.java
@@ -1,34 +1,23 @@
-import java.util.Scanner;
-
 public class Game {
-    public static void main(String[] args){
-        Player p = new Player("Hero", 50);
-        Monster m = new Monster("Slime", 30);
-        Scanner sc = new Scanner(System.in);
 
-        while(p.isAlive() && m.isAlive()){
-            System.out.println(p.getStatus());
-            System.out.println(m.getStatus());
-            System.out.println("1) Attack 2) Heal");
-            int choice = sc.nextInt();
-            if(choice == 1){
-                int dmg = p.attack();
-                m.takeDamage(dmg);
-                System.out.println("You deal " + dmg + " damage!");
-            } else {
-                p.heal(Dice.roll(5,10));
-                System.out.println("You heal yourself!");
-            }
+    public static void main(String[] args) {
+        GameEngine engine = new GameEngine();
+        engine.startGame();
+    }
+}
 
-            if(m.isAlive()){
-                int dmg = m.attack();
-                p.takeDamage(dmg);
-                System.out.println(m.name + " attacks for " + dmg + " damage!");
-            }
-        }
+class GameEngine {
 
-        if(p.isAlive()) System.out.println("You win!");
-        else System.out.println("You lose!");
-        sc.close();
+    Player player;
+    Enemy enemy;
+    BattleSystem battle;
+
+    public void startGame() {
+
+        player = new Player("Hero", 100);
+        enemy = new Goblin();
+
+        battle = new BattleSystem();
+        battle.startBattle(player, enemy);
     }
 }
